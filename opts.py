f245eb178ed2300047137a6b92b259c3fb965d93
Serena Shi <1961187023@qq.com>
2025-11-26 23:16:40 +0800

    updated

--- opts.py
+++ opts.py
@@ -68,8 +68,8 @@ parser.add_argument('--flow_prefix', default="", type=str)
 parser.add_argument('--root_log',type=str, default='log')
 parser.add_argument('--root_model', type=str, default='checkpoint')
 
-# parser.add_argument('--shift', default=False, action="store_true", help='use shift for models')
-parser.add_argument('--shift', default=False, help='use shift for models')
+parser.add_argument('--shift', default=False, action="store_true", help='use shift for models')
+#parser.add_argument('--shift', default=False, help='use shift for models')
 parser.add_argument('--shift_div', default=8, type=int, help='number of div for shift (default: 8)')
 parser.add_argument('--shift_place', default='blockres', type=str, help='place for shift (default: stageres)')
 
